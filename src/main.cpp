178ecc0ae711a7b3ba425641dc4f75cbe33542b6
Wojciech Ossowski <zavafoj@gmail.com>
2018-11-10 12:26:17 +0100

    #18: Reorganizing main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -53,9 +53,9 @@ using namespace Core::Api::Messages;
     #include <windows.h>
 #endif
 
-QFile debugFile;
-QMutex debugMutex;
-QTextStream debugStream;
+static QFile debugFile;
+static QMutex debugMutex;
+static QTextStream debugStream;
 
 void handleMessage(QtMsgType type,
                    const QMessageLogContext &context,
@@ -98,8 +98,89 @@ void createDebugEnvironment(Api::ApiGateway &gateway, const QCommandLineParser &
 
     qDebug() << "Account" << (gateway.isConfigured() ? "configured!" : "unconfigured ;(");
 }
+
+void debugQuery()
+{
+
+}
 #endif
 
+void initializeCommandLineInterface(const QCoreApplication &application, QCommandLineParser &parser)
+{
+    parser.setApplicationDescription(qApp->translate("main", "Lazy farmer - My Free Farm bot"));
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addOptions({
+        { { "l", "login" },
+          qApp->translate("main", "Specifies login on startup."),
+          qApp->translate("main", "User's login.") },
+        { { "p", "password" },
+          qApp->translate("main", "Specifies password on startup."),
+          qApp->translate("main", "User's password.") },
+        { { "d", "domain" },
+          qApp->translate("main", "Specifies game's domain on startup."),
+          qApp->translate("main", "Domain (myfreefarm.de / wolnifarmerzy.pl).") },
+        { { "s", "server" },
+          qApp->translate("main", "Specifies server number on startup."),
+          qApp->translate("main", "Server number.") },
+        { { "c", "config" },
+          qApp->translate("main", "Specifies config file on startup."),
+          qApp->translate("main", "Task manager's configuration file.") },
+        { { "n", "no-gui" },
+          qApp->translate("main", "Disables UI Mode.") }
+    });
+    // Execute CLI Parser
+    parser.process(application);
+}
+
+void initializeLoggingEnvironment()
+{
+    // Inilialize cache directory
+    QDir applicationDir = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    if (!applicationDir.mkpath("."))
+        throw std::ios_base::failure(qApp->translate("main", "Unable to create path to application data.").toStdString());
+
+    // Initalize log directory
+    const QString fileName = QDateTime::currentDateTime().toString("yyyy_MM_dd-HH_mm.log");
+    debugFile.setFileName(applicationDir.absoluteFilePath(fileName));
+
+    // Create debug prompt device
+    debugStream.setDevice(&debugFile);
+    if (!debugFile.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Unbuffered))
+        throw std::ios_base::failure(qApp->translate("main", "Unable to open logging file.").toStdString());
+
+    // Initialize message handler
+    qSetMessagePattern("[%{time HH:mm:ss.zzz}] %{type}: %{message} (%{function}, %{file}, %{line})");
+    qInstallMessageHandler(handleMessage);
+
+    qDebug() << debugFile.fileName();
+
+}
+
+void initializeStaticGameData()
+{
+#ifdef DEBUG_MODE
+    QDir assetsDirectory(ASSETS_DIRECTORY);
+#else
+    QDir assetsDirectory(qApp->applicationDirPath());
+#endif
+    QFile buildingConfig(assetsDirectory.absoluteFilePath("building-config.json"));
+    if (buildingConfig.open(QIODevice::ReadOnly)) {
+        if (!GlobalGameData::loadBuildingTypes(buildingConfig.readAll())) {
+            throw std::ios_base::failure(qApp->translate("main", "Unable to read building-config.json").toStdString());
+        }
+    } else {
+        throw std::ios_base::failure(qApp->translate("main", "Unable to load building-config.json").toStdString());
+    }
+}
+
+void registerCustomMetatypes()
+{
+    qRegisterMetaType<Core::Data::BuildingType>("Core::Data::BuildingType");
+    qRegisterMetaType<Core::Data::BuildingDetails>("Core::Data::BuildingDetails");
+    qRegisterMetaType<Core::Data::ProductDetails>("Core::Data::ProductDetails");
+    qRegisterMetaType<Core::Data::ProductionDetails>("Core::Data::ProductionDetails");
+}
 
 int main(int argc, char *argv[])
 {
@@ -125,84 +206,16 @@ int main(int argc, char *argv[])
 
     // Initialize command-line parser
     QCommandLineParser parser;
-    parser.setApplicationDescription(qApp->translate("main", "Lazy farmer - My Free Farm bot"));
-    parser.addHelpOption();
-    parser.addVersionOption();
-    parser.addOptions({
-        { { "l", "login" },
-          qApp->translate("main", "Specifies login on startup."),
-          qApp->translate("main", "User's login.") },
-        { { "p", "password" },
-          qApp->translate("main", "Specifies password on startup."),
-          qApp->translate("main", "User's password.") },
-        { { "d", "domain" },
-          qApp->translate("main", "Specifies game's domain on startup."),
-          qApp->translate("main", "Domain (myfreefarm.de / wolnifarmerzy.pl).") },
-        { { "s", "server" },
-          qApp->translate("main", "Specifies server number on startup."),
-          qApp->translate("main", "Server number.") },
-        { { "c", "config" },
-          qApp->translate("main", "Specifies config file on startup."),
-          qApp->translate("main", "Task manager's configuration file.") },
-        { { "n", "no-gui" },
-          qApp->translate("main", "Disables UI Mode.") }
-    });
-    // Execute CLI Parser
-    parser.process(lazyFarmerApp);
+    initializeCommandLineInterface(lazyFarmerApp, parser);
 
     try {
-        // Inilialize cache directory
-        QDir applicationDir = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
-        if (!applicationDir.mkpath("."))
-            throw std::ios_base::failure(qApp->translate("main", "Unable to create path to application data.").toStdString());
-
-        // Initalize log directory
-        const QString fileName = QDateTime::currentDateTime().toString("yyyy_MM_dd-HH_mm.log");
-        debugFile.setFileName(applicationDir.absoluteFilePath(fileName));
-
-        // Create debug prompt device
-        debugStream.setDevice(&debugFile);
-        if (!debugFile.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Unbuffered))
-            throw std::ios_base::failure(qApp->translate("main", "Unable to open logging file.").toStdString());
-
-        // Initialize message handler
-        qSetMessagePattern("[%{time HH:mm:ss.zzz}] %{type}: %{message} (%{function}, %{file}, %{line})");
-        qInstallMessageHandler(handleMessage);
-
-        qDebug() << debugFile.fileName();
-
-        // Load Static game data
-
-#ifdef DEBUG_MODE
-        QDir assetsDirectory(ASSETS_DIRECTORY);
-#else
-        QDir assetsDirectory(qApp->applicationDirPath());
-#endif
-        QFile buildingConfig(assetsDirectory.absoluteFilePath("building-config.json"));
-        if (buildingConfig.open(QIODevice::ReadOnly)) {
-            if (!GlobalGameData::loadBuildingTypes(buildingConfig.readAll())) {
-                throw std::ios_base::failure(qApp->translate("main", "Unable to read building-config.json").toStdString());
-            }
-        } else {
-            throw std::ios_base::failure(qApp->translate("main", "Unable to load building-config.json").toStdString());
-        }
-
-        // Load custom metatypes
-        qRegisterMetaType<Core::Data::BuildingType>("Core::Data::BuildingType");
-        qRegisterMetaType<Core::Data::BuildingDetails>("Core::Data::BuildingDetails");
-        qRegisterMetaType<Core::Data::ProductDetails>("Core::Data::ProductDetails");
-        qRegisterMetaType<Core::Data::ProductionDetails>("Core::Data::ProductionDetails");
-
+        initializeLoggingEnvironment();
+        initializeStaticGameData();
+        registerCustomMetatypes();
     } catch (std::exception &e) {
         qCritical() << e.what();
         return -1;
     }
-
-    if (parser.isSet("no-gui")) {
-        return lazyFarmerApp.exec();
-    }
-
-    QQmlApplicationEngine engine;
 
 #ifdef DEBUG_MODE
     qDebug() << "Debug storage located in:" << QFileInfo(debugFile).absoluteFilePath();
@@ -212,16 +225,27 @@ int main(int argc, char *argv[])
     createDebugEnvironment(debugGateway, parser);
 
     const auto getInfo = [&](){
+        QVector<int> fields {};
         debugGateway.queueMessage(GetFarmInfo::Ptr(new GetFarmInfo(&debugGateway)));
+
+        for (int i = 0; i < 120; i++) {
+            const Data::BuildingDetails buildingDetails = {1, 1};
+            const Data::ProductDetails produceDetails = {17, 1, i};
+            debugGateway.queueMessage(GetCollect::Ptr(new GetCollect(&debugGateway, buildingDetails, produceDetails)));
+            debugGateway.queueMessage(SetPlant::Ptr(new SetPlant(&debugGateway, buildingDetails, produceDetails)));
+            debugGateway.queueMessage(SetPour::Ptr(new SetPour(&debugGateway, buildingDetails, produceDetails)));
+        }
         debugGateway.start();
     };
 
     getInfo();
-
-    Model::StorageModel storageModel(p.storage());
-
-    engine.rootContext()->setContextProperty("StorageModel", &storageModel);
 #endif
+
+    if (parser.isSet("no-gui")) {
+        return lazyFarmerApp.exec();
+    }
+
+    QQmlApplicationEngine engine;
 
     engine.load(QUrl(QLatin1String("qrc:/qml/main.qml")));
     if (engine.rootObjects().isEmpty()){
